d22fca579231c243f0eeec3e742cd733b75cf556
Junaid Arshad <junaidarshad439@gmail.com>
2025-01-02 15:18:29 +0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Todos from "./components/Todos"
+import './App.css'
+import AddTodo from "./components/Addtodo"
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+      
+    </>
+  )
+}
+
+export default App
